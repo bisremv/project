17a1e8cd449a9f93355de17481699e949be6190e
bisrat yared <bisremv@gmail.com>
2023-07-01 14:57:00 +0300

    app calander on development may add some features in the end

--- lib/FeturedImg.dart
+++ lib/FeturedImg.dart
@@ -8,44 +8,6 @@ class FeaturedImg extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Padding(
-      padding: const EdgeInsets.all(8.0),
-      child: Container(
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            Text("$imgName"),
-            SizedBox(
-              height: 10,
-            ),
-            Container(
-              decoration: BoxDecoration(
-                color: Colors.white,
-                borderRadius: BorderRadius.all(
-                  Radius.circular(10),
-                ),
-                boxShadow: [
-                  BoxShadow(
-                    color: Colors.grey.withOpacity(0.5),
-                    spreadRadius: 5,
-                    blurRadius: 7,
-                    offset: Offset(0, 3), // changes position of shadow
-                  ),
-                ],
-              ),
-              child: ClipRRect(
-                borderRadius: BorderRadius.circular(14.0),
-                child: Image.asset(
-                  imgUrl,
-                  width: 170,
-                  height: 200,
-                  fit: BoxFit.cover,
-                ),
-              ),
-            ),
-          ],
-        ),
-      ),
-    );
+    return Container();
   }
 }
--- lib/Home.dart
+++ lib/Home.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:project/FeturedImg.dart';
+import 'package:scroll_snap_list/scroll_snap_list.dart';
 
 class Home extends StatefulWidget {
   const Home({super.key});
@@ -11,6 +12,12 @@ int _index = 0;
 
 class _HomeState extends State<Home> {
   int _selectedIndex = 0;
+
+  String Placename = "Place Name";
+  String PlaceLocation = "country city";
+  int like = 0;
+  double Rating = 4.2;
+  int Comment = 0;
   void _onItemTapped(int index) {
     setState(() {
       _selectedIndex = index;
@@ -23,11 +30,143 @@ class _HomeState extends State<Home> {
     });
   }
 
+  void _onItemFocus(int index) {
+    setState(() {
+      // _focusedIndex = index;
+    });
+  }
+
+  List imgPlace = [
+    // "img/Ellipse 2.png",
+    "img/image3.png",
+    "img/image1.png",
+    "img/Ethiopia-attractions-860x400.jpg",
+    "img/image3.png",
+    "img/Axum-the-cradel-of-Ethiopiac-civ.png",
+    "img/Ellipse 2.png",
+    "img/image3.png",
+    "img/image1.png",
+    "img/Ethiopia-attractions-860x400.jpg",
+    "img/image3.png",
+    "img/Axum-the-cradel-of-Ethiopiac-civ.png",
+    "img/Ellipse 2.png",
+    "img/image3.png",
+    "img/image1.png",
+    "img/Ethiopia-attractions-860x400.jpg",
+    "img/image3.png",
+    "img/Axum-the-cradel-of-Ethiopiac-civ.png",
+    "img/blue-nile-water-falls.png"
+  ];
   TextStyle Txtstyhead = TextStyle(
     color: Colors.black,
     fontWeight: FontWeight.bold,
-    fontSize: 42,
+    fontSize: 38,
   );
+
+  Widget _buildListItem(BuildContext context, int index) {
+    String imgPath = imgPlace[index];
+    return SizedBox(
+      width: 400,
+      child: Stack(
+        // mainAxisAlignment: MainAxisAlignment.center,
+        // crossAxisAlignment: CrossAxisAlignment.center,
+        children: <Widget>[
+          Center(
+            child: ClipPath(
+              child: Container(
+                height: 200,
+                decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(15),
+                    color: Colors.transparent),
+                child: ClipRRect(
+                  borderRadius: BorderRadius.circular(35.0),
+                  child: Card(
+                    // color: Colors.transparent,
+                    elevation: 6,
+                    //add border radius
+                    child: Image.asset(imgPath),
+                  ),
+                ),
+              ),
+            ),
+          ),
+          Positioned(
+            bottom: 12,
+            left: 73,
+            child: Container(
+              alignment: Alignment.bottomCenter,
+              width: 260,
+              height: 80,
+              decoration: BoxDecoration(
+                color: Colors.white.withOpacity(0.75),
+                borderRadius: BorderRadius.circular(15),
+              ),
+              child: Column(
+                // mainAxisAlignment: MainAxisAlignment.start,
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Padding(
+                    padding: const EdgeInsets.fromLTRB(15, 4, 0, 0),
+                    child: Text(Placename),
+                  ),
+                  Row(
+                    children: [
+                      Icon(
+                        Icons.location_on_outlined,
+                        size: 20,
+                      ),
+                      Text(PlaceLocation),
+                    ],
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.fromLTRB(10, 0, 0, 6),
+                    child: Row(
+                      children: [
+                        Icon(
+                          Icons.thumb_up,
+                          size: 22,
+                        ),
+                        Text(" $like"),
+                        SizedBox(
+                          width: 15,
+                        ),
+                        Icon(
+                          Icons.comment,
+                          size: 20,
+                        ),
+                        SizedBox(
+                          width: 5,
+                        ),
+                        Text("$Comment"),
+                        SizedBox(
+                          width: 65,
+                        ),
+                        Column(
+                          children: [
+                            Text("Rating"),
+                            Row(
+                              children: [
+                                Icon(
+                                  Icons.star,
+                                  size: 15,
+                                ),
+                                Text("$Rating")
+                              ],
+                            )
+                          ],
+                        )
+                      ],
+                    ),
+                  )
+                ],
+              ),
+            ),
+          )
+        ],
+      ),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -42,21 +181,142 @@ class _HomeState extends State<Home> {
               style: Txtstyhead,
             ),
             Container(
-              width: 370,
-              height: 300,
-              color: Colors.red,
-              child: ListView(
-                padding: const EdgeInsets.all(8),
-                scrollDirection: Axis.horizontal,
-                children: <Widget>[
-                  Padding(
-                    padding: const EdgeInsets.all(8.0),
-                    child: Container(
-                      child: Image.asset("img/blue-nile-water-falls.png"),
-                    ),
-                  ),
-                ],
+              // margin: EdgeInsets.all(8),
+              width: double.infinity,
+              height: 200,
+
+              child: ScrollSnapList(
+                itemBuilder: _buildListItem,
+                itemCount: imgPlace.length,
+                itemSize: 400,
+                allowAnotherDirection:
+                    axisDirectionIsReversed(AxisDirection.down),
+                onItemFocus: _onItemFocus,
+                dynamicItemSize: true,
               ),
+            ),
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Container(
+                // color: Colors.green,
+                width: double.infinity,
+                height: 30,
+                child: ListView(
+                  scrollDirection: Axis.horizontal,
+                  children: [
+                    ElevatedButton(
+                      onPressed: () {
+                        // Button action
+                      },
+                      style: ElevatedButton.styleFrom(
+                        elevation: 6,
+                        shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(8.0),
+                        ),
+                      ),
+                      child: Text(
+                        'Text',
+                        style: TextStyle(
+                          fontSize: 16,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                    ),
+                    SizedBox(
+                      width: 15,
+                    ),
+                    ElevatedButton(
+                      onPressed: () {
+                        // Button action
+                      },
+                      style: ElevatedButton.styleFrom(
+                        elevation: 6,
+                        shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(8.0),
+                        ),
+                      ),
+                      child: Text(
+                        'Text',
+                        style: TextStyle(
+                          fontSize: 16,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                    ),
+                    SizedBox(
+                      width: 15,
+                    ),
+                    ElevatedButton(
+                      onPressed: () {
+                        // Button action
+                      },
+                      style: ElevatedButton.styleFrom(
+                        elevation: 6,
+                        shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(8.0),
+                        ),
+                      ),
+                      child: Text(
+                        'Text',
+                        style: TextStyle(
+                          fontSize: 16,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                    ),
+                    SizedBox(
+                      width: 15,
+                    ),
+                    ElevatedButton(
+                      onPressed: () {
+                        // Button action
+                      },
+                      style: ElevatedButton.styleFrom(
+                        elevation: 6,
+                        shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(8.0),
+                        ),
+                      ),
+                      child: Text(
+                        'Text',
+                        style: TextStyle(
+                          fontSize: 16,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                    ),
+                    SizedBox(
+                      width: 15,
+                    ),
+                    ElevatedButton(
+                      onPressed: () {
+                        // Button action
+                      },
+                      style: ElevatedButton.styleFrom(
+                        elevation: 6,
+                        shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(8.0),
+                        ),
+                      ),
+                      child: Text(
+                        'Text',
+                        style: TextStyle(
+                          fontSize: 16,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                    ),
+                    SizedBox(
+                      width: 15,
+                    ),
+                  ],
+                ),
+              ),
+            ),
+            Container(
+              color: Colors.red,
+              width: double.infinity,
+              height: 300,
             )
           ]),
         ),
